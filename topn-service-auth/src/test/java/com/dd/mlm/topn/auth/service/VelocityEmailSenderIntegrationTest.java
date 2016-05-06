ab7c695adf92c626ac847f072f9338de89d78fdf
lordoftheflies <heglas11@gmail.com>
2016-05-06 22:52:38 +0200

    Fix tests.

--- topn-service-auth/src/test/java/com/dd/mlm/topn/auth/service/VelocityEmailSenderIntegrationTest.java
+++ topn-service-auth/src/test/java/com/dd/mlm/topn/auth/service/VelocityEmailSenderIntegrationTest.java
@@ -64,14 +64,14 @@ public class VelocityEmailSenderIntegrationTest {
 
 //        sender.sendRegistrationActivationEmail(INVITER_NAME, USER_EMAIL, USER_NAME, APPLICATION_NAME, ORGANOIZATION_NAME, REGISTRATION_LINK, ANDROID_INSTALLER_LINK, "en");
 //        sender.sendRegistrationActivationEmail(INVITER_NAME, USER_EMAIL, USER_NAME, APPLICATION_NAME, ORGANOIZATION_NAME, REGISTRATION_LINK, ANDROID_INSTALLER_LINK, "hu");
-//        sender.sendRegistrationActivationEmail(INVITER_NAME, USER_EMAIL, USER_NAME, APPLICATION_NAME, ORGANOIZATION_NAME, REGISTRATION_LINK, ANDROID_INSTALLER_LINK, "po");
+        sender.sendRegistrationActivationEmail(INVITER_NAME, USER_EMAIL, USER_NAME, APPLICATION_NAME, ORGANOIZATION_NAME, REGISTRATION_LINK, ANDROID_INSTALLER_LINK, "po");
     }
-    public static final String REGISTRATION_LINK = "http://185.51.67.30/registrate?=c0b1f132-1220-11e6-a148-3e1d05defe78";
+    public static final String REGISTRATION_LINK = "http://185.51.67.30/registrate?=3c6c3f77-1a8d-425e-b8e6-07c7a115b3a9";
     public static final String ANDROID_INSTALLER_LINK = "http://play.google.com/topflavon";
     public static final String ORGANOIZATION_NAME = "TopFlavon Team";
     public static final String APPLICATION_NAME = "TopFlavon";
     public static final String USER_NAME = "Péczely Balázs";
     public static final String INVITER_NAME = "Hegedűs László";
-//    public static final String USER_EMAIL = "peczely.balazs@digitaldefense.hu";
-    public static final String USER_EMAIL = "heglas11@gmail.com";
+    public static final String USER_EMAIL = "peczely.balazs@digitaldefense.hu";
+//    public static final String USER_EMAIL = "heglas11@gmail.com";
 }
